5c04195da0a87caeeb440037d4723675cfdeda95
Ukobo Utibeabasi <ukoboutibe@yahoo.com>
2025-01-31 19:24:47 -0500

    second

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server Started on http://localhost:${port}`);
 });
-
-// mongodb+srv://utibeukobo14:utibeukobo@cluster0.bli3i.mongodb.net/?
